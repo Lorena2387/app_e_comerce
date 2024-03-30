579834305369bdddfa974450ebc20ab9ccc408ad
Lorena2387 <lorenaramirez23@hotmail.com>
2024-03-29 19:49:39 -0500

    implement image of plates of food and a container on top

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -8,11 +8,13 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85);
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 320,
       child: PageView.builder(
+          controller: pageController,
           itemCount: 5,
           itemBuilder: (context, position) {
             return _buildPageItem(position);
@@ -21,13 +23,32 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Container(
-      height: 220,
-      margin: EdgeInsets.only(left: 5, right: 5),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color: Color(0xFF69c5df),
-      ),
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: EdgeInsets.only(left: 5, right: 5),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage("asset/images/food1.png"),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 140,
+            margin: EdgeInsets.only(left: 20, right: 20, bottom: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
