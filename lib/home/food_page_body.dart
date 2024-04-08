d261da4c808f97d0286d1b76c9e3ef671578226e
Lorena2387 <lorenaramirez23@hotmail.com>
2024-04-08 13:44:24 -0500

    implement listViw.builder and place images inside a container

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -67,12 +67,16 @@ class _FoodPageBodyState extends State<FoodPageBody> {
         Container(
           margin: EdgeInsets.only(left: Dimensions.width30),
           child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               BigText(text: "Popular"),
               SizedBox(
                 width: Dimensions.width10,
               ),
               Container(
+                margin: const EdgeInsets.only(
+                  bottom: 3,
+                ),
                 child: BigText(
                   text: ".",
                   color: Colors.black26,
@@ -81,8 +85,42 @@ class _FoodPageBodyState extends State<FoodPageBody> {
               SizedBox(
                 width: Dimensions.width10,
               ),
+              Container(
+                margin: const EdgeInsets.only(
+                  bottom: 4,
+                ),
+                child: SmallText(text: "Food pairing"),
+              )
             ],
           ),
+        ),
+        //list of food and images
+        Container(
+          height: 900,
+          child: ListView.builder(
+              physics: AlwaysScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.only(
+                      left: Dimensions.width20, right: Dimensions.width20),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 120,
+                        height: 120,
+                        decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.circular(Dimensions.radius20),
+                            color: Colors.white38,
+                            image: DecorationImage(
+                                image: AssetImage("asset/images/food0.png"))),
+                      )
+                    ],
+                  ),
+                );
+              }),
         )
       ],
     );
--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -64,7 +64,10 @@ class _MainFoodPageState extends State<MainFoodPage> {
             ),
           ),
         ),
-        FoodPageBody()
+        Expanded(
+            child: SingleChildScrollView(
+          child: FoodPageBody(),
+        ))
       ],
     ));
   }
