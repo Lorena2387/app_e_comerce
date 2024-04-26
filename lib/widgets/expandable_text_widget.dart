75055d228ee2466770c3393e87f8cf8fd079e710
Lorena2387 <lorenaramirez23@hotmail.com>
2024-04-25 19:50:30 -0500

    define conditions to split the text into two parts

--- lib/widgets/expandable_text_widget.dart
+++ lib/widgets/expandable_text_widget.dart
@@ -1,3 +1,4 @@
+import 'package:app_e_comerce/Utils/dimensions.dart';
 import 'package:flutter/material.dart';
 
 class ExpandableTextWidget extends StatefulWidget {
@@ -13,8 +14,24 @@ class _ExpandableTextWidgetState extends State<ExpandableTextWidget> {
   late String secondHalf;
 
   bool hiddenText = true;
+
+  double textHeigth = Dimensions.screenHeight / 5.63;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.text.length > textHeigth) {
+      firstHalf = widget.text.substring(0, textHeigth.toInt());
+      secondHalf =
+          widget.text.substring(textHeigth.toInt() + 1, widget.text.length);
+    } else {
+      firstHalf = widget.text;
+      secondHalf = "";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container();
   }
 }
