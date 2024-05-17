c66b51e30d664f874eb279bd93dccef1e387ba15
Lorena2387 <lorenaramirez23@hotmail.com>
2024-05-17 13:21:09 -0500

    implement a container at the top below the image with a title and the container can be moved with the description of the dish

--- lib/pages/food/recommended_food_detail.dart
+++ lib/pages/food/recommended_food_detail.dart
@@ -1,3 +1,7 @@
+import 'package:app_e_comerce/Utils/app_colors.dart';
+import 'package:app_e_comerce/Utils/dimensions.dart';
+import 'package:app_e_comerce/widgets/app_icon.dart';
+import 'package:app_e_comerce/widgets/big_text.dart';
 import 'package:flutter/material.dart';
 
 class RecommendedFoodDetail extends StatelessWidget {
@@ -9,6 +13,26 @@ class RecommendedFoodDetail extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(icon: Icons.clear),
+                AppIcon(icon: Icons.shopping_cart_outlined)
+              ],
+            ),
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(0),
+              child: Container(
+                color: Colors.white,
+                child: Center(
+                    child: BigText(size: Dimensions.font26, text: "Pea dish")),
+                width: double.maxFinite,
+                padding: EdgeInsets.only(top: 5, left: 10),
+              ),
+            ),
+            pinned: true,
+            backgroundColor: AppColors.yellowColor,
+            expandedHeight: 300,
             flexibleSpace: FlexibleSpaceBar(
               background: Image.asset(
                 "asset/images/food0.png",
@@ -16,7 +40,11 @@ class RecommendedFoodDetail extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
             ),
-          )
+          ),
+          SliverToBoxAdapter(
+            child: Text(
+                "Prepara nuestras genuinas Arvejas verdes partidas premium, seleccionados manualmente para darte la mejor calidad y en su envase 100% reciclable más amigable con el medio ambiente.Las arvejas verdes de Iansa Agro son una excelente fuente de proteína vegetal , te recomendamos consumirlas con cereales para lograr una proteína de alto valor biológico. Además son una buena fuente de fibra dietética, otorgando una sensación de saciedad prolongada. También contienen minerales esenciales para nuestro organismo como el hierro, necesario para transportar oxígeno a todo nuestro cuerpo, fósforo y zinc. Conoce los detalles de cada nutriente que aportan las arvejas verdes partidas de Iansa Agro a tu dieta en la tabla con información nutricional en esta página. Prepara nuestras genuinas Arvejas verdes partidas premium, seleccionados manualmente para darte la mejor calidad y en su envase 100% reciclable más amigable con el medio ambiente.Las arvejas verdes de Iansa Agro son una excelente fuente de proteína vegetal , te recomendamos consumirlas con cereales para lograr una proteína de alto valor biológico. Además son una buena fuente de fibra dietética, otorgando una sensación de saciedad prolongada. También contienen minerales esenciales para nuestro organismo como el hierro, necesario para transportar oxígeno a todo nuestro cuerpo, fósforo y zinc. Conoce los detalles de cada nutriente que aportan las arvejas verdes partidas de Iansa Agro a tu dieta en la tabla con información nutricional en esta página."),
+          ),
         ],
       ),
     );
