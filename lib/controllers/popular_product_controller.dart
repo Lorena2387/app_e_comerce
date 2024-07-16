9a36e83fd1c475db6bcc572aa1aa50716758fd3e
Lorena2387 <lorenaramirez23@hotmail.com>
2024-07-15 21:30:21 -0500

    building drivers

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -1,3 +1,15 @@
 import 'package:get/get.dart';
 
-class PopularProductController extends GetxController {}
+import '../data/repository/popular_product_repo.dart';
+
+class PopularProductController extends GetxController {
+  final PopularProductRepo popularProductRepo;
+
+  PopularProductController({required this.popularProductRepo});
+
+  List<dynamic> _popularProductList = [];
+
+  Future<void> getPopularProductList() async {
+    Response response = await popularProductRepo.getPopularProductList();
+  }
+}
